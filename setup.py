0c6d8c81b60a0ff5c1cc3382748a12869fd16054
Javier Canovas <me@jlcanovas.es>
2023-02-27 11:01:29 +0100

    Adressing #1

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
         # your project is installed. For an analysis of "install_requires" vs pip's
         # requirements files see:
         # https://packaging.python.org/en/latest/requirements.html
-        install_requires=['mysql-connector', 'request'],
+        install_requires=['mysql-connector', 'requests'],
 
         # List additional groups of dependencies here (e.g. development
         # dependencies). You can install these using the following syntax,
